601606c6bd2ef8a1d4d3692e9c89d70cd3a30712
Suleymanozkan1 <98687117+Suleymanozkan1@users.noreply.github.com>
2022-03-05 23:54:46 +0300

    Create tr.ts

---
+++ src/assets/locales/tr.ts
@@ -0,0 +1,35 @@
+import type { TranslateState } from "../../context/TranslateProvider/model";
+
+const trTranslate: TranslateState = {
+  common: {
+    title_main: "StarkNet devrimi hayata geçiyor",
+    subtitle_main: "StarkNet L2'de şu ana kadar yapılmış ve yapılacak olan bütün projeleri keşfedin",
+    community_driven: "✨ Topluluk odaklı bir girişim",
+
+    no_project: "😕 Bu kategoride bir proje yok",
+    maybe_yours: "Belki de seninki olabilir",
+    made_with: "Made with",
+    by: "by",
+    alpha_road_team: "Alpha Road Team",
+    apply: "Başvur",
+    community: "Topluluk",
+  },
+  tags: {
+    all: "Tümü",
+    bridge: "Köprü",
+    dao: "DAO",
+    defi: "DeFi",
+    digital_id: "Dijital Kimlik",
+    gamefi: "GameFi",
+    governance: "Yönetişim",
+    infrastructure: "Altyapı",
+    nft: "NFT",
+    cefi: "CeFi",
+    payments: "Ödeme",
+    social: "Sosyal",
+    tools: "Araçlar",
+    wallet: "Cüzdan",
+  },
+};
+
+export default trTranslate;
