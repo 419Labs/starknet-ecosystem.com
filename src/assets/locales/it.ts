a26bc7e839cca982255b3ce0af7eff6c9962e136
hacktar <ugo@inzhoop.com>
2022-03-06 01:27:31 +0100

    :sparkles: Add IT translation
    Italian localization

---
+++ src/assets/locales/it.ts
@@ -0,0 +1,36 @@
+import type { TranslateState } from "../../context/TranslateProvider/model";
+
+const enTranslate: TranslateState = {
+  common: {
+    title_main: "La rivoluzione di StarkNet sta venendo alla luce del giorno",
+    subtitle_main: "Esplora tutti i progetti realizzati e in esecuzione su StarkNet L2",
+    community_driven: "✨ Un'iniziativa guidata dalla comunità",
+
+    no_project: "😕 Nessun progetto in questa categoria",
+    maybe_yours: "Forse la tua?",
+    made_with: "Fatto con",
+    by: "da",
+    alpha_road_team: "Alpha Road Team",
+    apply: "Applica",
+    community: "Comunità",
+  },
+  tags: {
+    all: "Tutti",
+    bridge: "Bridge",
+    dao: "DAO",
+    defi: "DeFi",
+    digital_id: "ID Digitale",
+    gamefi: "GameFi",
+    governance: "Governance",
+    infrastructure: "Infrastruttura",
+    nft: "NFT",
+    cefi: "CeFi",
+    payments: "Pagamenti",
+    social: "Social",
+    tools: "Strumenti",
+    wallet: "Wallet",
+    starkex: "StarkEx",
+  },
+};
+
+export default enTranslate;
