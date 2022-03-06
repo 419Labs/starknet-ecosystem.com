2d23aa2c401b2ecff62bfd9b8f0eb7fa572fbc38
izayl <izayl@163.com>
2022-03-07 01:00:48 +0800

    feat: add zh translate

---
+++ src/assets/locales/zh_TW.ts
@@ -0,0 +1,36 @@
+import type { TranslateState } from "../../context/TranslateProvider/model";
+
+const enTranslate: TranslateState = {
+  common: {
+    title_main: "StarkNet 革命即將到來",
+    subtitle_main: "探索所有在 StarkNet L2 上建立和運行的項目",
+    community_driven: "✨ 社區驅動的倡議",
+
+    no_project: "😕 該目錄下暫無項目",
+    maybe_yours: "提交你的項目 ？",
+    made_with: "Made with",
+    by: "by",
+    alpha_road_team: "Alpha Road Team",
+    apply: "添加項目",
+    community: "社區",
+  },
+  tags: {
+    all: "所有",
+    bridge: "橋",
+    dao: "DAO",
+    defi: "DeFi",
+    digital_id: "數字身份",
+    gamefi: "GameFi",
+    governance: "Governance",
+    infrastructure: "基礎設施",
+    nft: "NFT",
+    cefi: "CeFi",
+    payments: "支付",
+    social: "社交",
+    tools: "工具",
+    wallet: "錢包",
+    starkex: "StarkEx",
+  },
+};
+
+export default enTranslate;
