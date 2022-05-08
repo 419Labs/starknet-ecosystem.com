49f1f94bc8bf76d667c277347ca32a5bca1a1a6c
Florian Bellotti <florian.bellotti@viacesi.fr>
2022-05-08 14:10:06 +0200

    test, do not merge

--- data/company.ts
+++ data/company.ts
@@ -182,7 +182,19 @@ const allCompanies: Company[] = [
       telegram: "",
     },
   },
-
+  {
+    id: 15,
+    name: "Alpha Road test",
+    network: {
+      website: "https://alpharoad.fi",
+      github: "",
+      twitter: "https://twitter.com/alpharoad_fi",
+      medium: "https://alpharoad.medium.com/",
+      discord: "https://discord.gg/Bhfa3B2Gnq",
+      telegram: "https://t.me/alpharoad_fi",
+    },
+    logo: "alpharoad.png",
+  },
 
 ];
 
