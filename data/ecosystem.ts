3fa741db0344252246d259b72c6e15db450c2637
strangerstork <104856476+strangerstork@users.noreply.github.com>
2022-05-11 14:46:14 -0700

    🎨 Updating Stork Oracle (#59)

--- data/ecosystem.ts
+++ data/ecosystem.ts
@@ -1084,7 +1084,7 @@ export const allProjects: Array<Project> = [
     name: "Stork Oracle Network",
     short_name: "Stork",
     description:
-      "Stork is an oracle network for StarkEx and StarkNet, providing low latency off-chain price quotes from trusted sources.",
+      "Stork is an oracle network for StarkEx and StarkNet, optimized for DeFi and providing low latency off-chain price quotes from trusted sources.",
     tags: ["defi"],
     logo: "stork.png",
     network: {
