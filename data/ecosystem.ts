9431446d83b0e367f016a78474cfa459fcbf038c
Dex Chen | Verilog <104154097+Dex-C@users.noreply.github.com>
2022-05-31 13:55:15 -0700

    ✨ Introducing Verilog Solutions
    ✨ Introducing Verilog Solutions

--- data/ecosystem.ts
+++ data/ecosystem.ts
@@ -18,6 +18,24 @@ export interface ProjectItf extends Project {
 }
 
 export const allProjects: Array<Project> = [
+    {
+    name: "Verilog Solutions",
+    short_name: "Verilog",
+    description:
+      "Founded by a group of cryptography researchers and smart contract engineers in North America, Verilog Solutions elevates the security standard for Web3 ecosystems by being a full-stack Web3 security firm covering smart contract security, consensus security, and operational security for Web3 projects. Verilog Solutions team works closely with major ecosystems and Web3 projects and applies a quality above quantity approach with a continuous security model. Verilog Solutions onboards the best and most innovative projects and provides the best-in-class advisory service on security needs, including on-chain and off-chain components.",
+    tags: ["infrastructure"],
+    logo: "verilog.png",
+    network: {
+      website: "https://www.verilog.solutions/",
+      github: "",
+      twitter: "https://twitter.com/verilog_audit",
+      medium: "",
+      discord: "",
+      telegram: "",
+    },
+    isLive: true,
+    isTestnetLive: true,
+  },
   {
     name: "SithSwap",
     short_name: "SithSwap",
