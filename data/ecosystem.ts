52119e48820420b41a73d31bebfaf9284ed985e8
0xEvon <103539457+0xEvon@users.noreply.github.com>
2022-08-07 21:54:15 +0200

    Add StarkBoard
    Add StarkBoard, NO JPEG

--- data/ecosystem.ts
+++ data/ecosystem.ts
@@ -1514,4 +1514,22 @@ export const allProjects: Array<Project> = [
     isLive: false,
     isTestnetLive: true,
   },
+  {
+    name: "StarkBoard",
+    short_name: "StarkBoard",
+    description:
+      "Find all the latest Starknet data that you are looking for.",
+    tags: ["tools"],
+    logo: "",
+    network: {
+      website: "hhttps://www.starkboard.io/",
+      github: "",
+      twitter: "https://twitter.com/starkboard",
+      medium: "https://medium.com/@starkboard",
+      discord: "https://discord.com/invite/starkboard",
+      telegram: "",
+    },
+    isLive: false,
+    isTestnetLive: true,
+  },
 ];
