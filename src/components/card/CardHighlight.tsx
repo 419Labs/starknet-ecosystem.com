e8840375b93d8410043af0b63740f1fb1250bc50
0xlny <lenny@419labs.io>
2022-09-30 12:31:43 +0200

    Add example card highlighted

--- src/components/card/CardHighlight.tsx
+++ src/components/card/CardHighlight.tsx
@@ -1,32 +1,59 @@
-import { Flex, HStack, Text } from "@chakra-ui/layout";
+import { Box, Flex, HStack, Text, VStack } from "@chakra-ui/layout";
 
 interface CardHighlightProps {
   icon?: any;
   title: string;
   content: string;
+  bg?: string;
   color?: string;
 }
-function CardHighlight({ icon, title, content, color }: CardHighlightProps) {
+function CardHighlight({
+  icon,
+  title,
+  content,
+  bg,
+  color,
+}: CardHighlightProps) {
   return (
     <Flex
+      position="relative"
       direction="column"
       p={4}
       minW="250px"
       transition=".4s ease all"
       cursor="pointer"
       onClick={() => console.log("coucou")}
-      bg={color || "gray.600"}
+      bg={bg || "gray.600"}
       h="300px"
       w="250px"
       borderRadius="md"
     >
-      <HStack>
-        {icon}
-        <Text>{title}</Text>
+      <HStack overflow="hidden" align="flex-start">
+        <Box h="full" pt={1} color={color || "inherit"}>
+          {icon}
+        </Box>
+        <VStack pl={4} align="flex-start">
+          <Text fontSize="xl" fontWeight="bold" color={color || "inherit"}>
+            {title}
+          </Text>
+          <Text>{content}</Text>
+        </VStack>
       </HStack>
-      <Text overflow="hidden" textOverflow="ellipsis">
-        {content}
-      </Text>
+      <Box
+        top={0}
+        bottom={0}
+        left={0}
+        right={0}
+        position="absolute"
+        borderRadius="md"
+        sx={{
+          boxShadow: `inset 0 -46px 13px -10px ${
+            bg
+              ? `var(--chakra-colors-${bg.replace(".", "-")})`
+              : "var(--chakra-colors-gray-600)"
+          }`,
+        }}
+      />
     </Flex>
   );
 }
--- src/pages/academy/index.tsx
+++ src/pages/academy/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, SimpleGrid, Text, VStack } from "@chakra-ui/layout";
+import { Box, Flex, HStack, SimpleGrid, Text, VStack } from "@chakra-ui/layout";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { FC } from "react";
@@ -143,11 +143,20 @@ const AcademyPage: FC = () => {
           <Text pt={20} fontSize="6xl" fontWeight="bold">
             Useful
           </Text>
-          <CardHighlight
-            icon={<FontAwesomeIcon icon={solid("home")} />}
-            title="Get started"
-            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-          />
+          <HStack>
+            <CardHighlight
+              bg="flat.100"
+              icon={<FontAwesomeIcon icon={solid("home")} />}
+              title="Get started"
+              content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+            />
+            <CardHighlight
+              color="flat.100"
+              icon={<FontAwesomeIcon icon={solid("home")} />}
+              title="Get started"
+              content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+            />
+          </HStack>
         </VStack>
       </Flex>
     </Flex>
