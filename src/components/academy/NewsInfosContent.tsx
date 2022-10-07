33d747eb4dffb3a6890f9a14e13fa107bab7b580
0xlny <lenny@419labs.io>
2022-10-07 15:37:17 +0200

    Add explanation card to news & feed page

--- src/components/academy/NewsInfosContent.tsx
+++ src/components/academy/NewsInfosContent.tsx
@@ -1,4 +1,7 @@
-import { VStack } from "@chakra-ui/layout";
+import { Box, VStack } from "@chakra-ui/layout";
+import { useTheme } from "@chakra-ui/react";
+
+import CardExplanation from "../card/CardExplanation";
 
 import type { BasicContentProps } from "./BasicContent";
 import BasicContent from "./BasicContent";
@@ -8,8 +11,22 @@ function NewsInfosContent({
   keyword = "",
   observe,
 }: BasicContentProps) {
+  const theme = useTheme();
+  const getGradient = () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    return `radial-gradient(ellipse at top, ${theme.colors.flat[100]}, transparent), radial-gradient(ellipse at bottom, ${theme.colors.flat[700]}, transparent), radial-gradient(ellipse at center, ${theme.colors.flat[700]}, transparent)`;
+  };
   return (
     <VStack w="full" align="flex-start">
+      <Box w="full" mb={8}>
+        <CardExplanation
+          bg={getGradient()}
+          title="News & Feeds"
+          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam <br/>Lorem ipsum dolor sit amet"
+          link="https://www.google.com/"
+        />
+      </Box>
       <BasicContent resources={resources} observe={observe} keyword={keyword} />
     </VStack>
   );
