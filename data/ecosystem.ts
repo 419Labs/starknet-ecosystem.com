088ca1ebc2de751330d2886730fa6fdb2b6b4a17
possc <voandy57@gmail.com>
2023-02-02 11:15:16 +0700

    Revert "🎨 Updating Stark Sport"
    This reverts commit 4d1523a3e16a3d9cd12778a680b88ea07ec469a6.

--- data/ecosystem.ts
+++ data/ecosystem.ts
@@ -2148,7 +2148,7 @@ export const allProjects: Array<Project> = [
     name: "WIW Protocol",
     shortName: "WIW",
     description:
-      'WIW is a privacy-preserving on-chain identity protocol that curates openly accessible Web3 user reputations.',
+        'WIW is a privacy-preserving on-chain identity protocol that curates openly accessible Web3 user reputations.',
     tags: ["digital_id", "infrastructure", "tools"],
     image: "wiw.png",
     network: {
@@ -2167,7 +2167,7 @@ export const allProjects: Array<Project> = [
     name: "Dragon Quest",
     shortName: "Dragon Quest",
     description:
-      'Dragon Quest is a text-based RPG game.',
+        'Dragon Quest is a text-based RPG game.',
     tags: ["gamefi", "nft"],
     image: "",
     network: {
@@ -2187,12 +2187,12 @@ export const allProjects: Array<Project> = [
     shortName: "Stark Sport",
     description:
       'Starksport is a king sport inspired project built on Binance Smart Chain, StarkNet...',
-    tags: ["nft", "defi", "gamefi"],
+    tags: ["defi", "gamefi"],
     image: "starksport.png",
     network: {
-      website: "http://starksport.finance",
+      website: "https://nft-dev.starksport.io/",
       github: "",
-      twitter: "https://twitter.com/starkfinance",
+      twitter: "https://twitter.com/starksportbet",
       medium: "",
       discord: "https://discord.gg/rX38ZwbqKw",
       telegram: "https://t.me/starksportchanel",
