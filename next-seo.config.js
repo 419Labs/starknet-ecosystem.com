ceae2d41d09cd1c48c23ef2265f15fc60fca91fc
xmrjun <3211351132@qq.com>
2023-02-19 15:25:11 +0800

    “update“

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,30 +1,25 @@
-/** @type {import('next-seo').DefaultSeoProps} */
 const defaultSEOConfig = {
-  title:
-    "Starknet Ecosystem | Discover the future of Ethereum scalability.",
-  titleTemplate: "%s",
-  defaultTitle:
-    "Explore projects, teams, jobs, metrics and everything around Starknet Ecosystem",
-  description:
-    "Explore projects, teams, jobs, metrics and everything around Starknet Ecosystem",
-  canonical: "https://starknet-ecosystem.com",
+  title: 'Starknet Ecosystem | Discover the future of Ethereum scalability.',
+  titleTemplate: '%s',
+  defaultTitle: 'Explore projects, teams, jobs, metrics and everything around Starknet Ecosystem',
+  description: 'Explore projects, teams, jobs, metrics and everything around Starknet Ecosystem',
+  canonical: 'https://starknet-ecosystem.com',
   openGraph: {
-    url: "https://starknet-ecosystem.com",
-    title: "Starknet Ecosystem",
-    description:
-      "Explore projects, teams, jobs, metrics and everything around Starknet Ecosystem",
+    url: 'https://starknet-ecosystem.com',
+    title: 'Starknet Ecosystem',
+    description: 'Explore projects, teams, jobs, metrics and everything around Starknet Ecosystem',
     images: [
       {
-        url: "https://www.starknet-ecosystem.com/starknet-map.png",
-        alt: "Starknet Ecosystem logo",
-      },
+        url: 'https://www.starknet-ecosystem.com/starknet-map.png',
+        alt: 'Starknet Ecosystem logo'
+      }
     ],
-    site_name: "Starknet Ecosystem",
+    site_name: 'Starknet Ecosystem'
   },
   twitter: {
-    cardType: "summary_large_image",
-    site: "@StarknetEco",
-  },
+    cardType: 'summary_large_image',
+    site: '@StarknetEco'
+  }
 };
 
 export default defaultSEOConfig;
