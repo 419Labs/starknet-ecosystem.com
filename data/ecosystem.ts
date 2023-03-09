304765b0b121f0afdac9cbda5eaa40c6f8daa349
bal10suz <95695907+bal10suz@users.noreply.github.com>
2023-03-09 22:31:31 +0300

    Update ecosystem.ts

--- data/ecosystem.ts
+++ data/ecosystem.ts
@@ -35,8 +35,7 @@ export const allProjects: Array<Project> = [
     id: "51e35a90-b7eb-4fbf-ba7e-2c19bf645b9c",
     name: "Foxes NFT",
     shortName: "Foxes NFT",
-    description:
-      "500 free NFTs created to increase community interaction in the Starknet NFT ecosystem",
+    description: "500 free NFTs created to increase community interaction in the Starknet NFT ecosystem",
     tags: ["NFT", "DAO"],
     image: "foxesnft.jpg",
     network: {
