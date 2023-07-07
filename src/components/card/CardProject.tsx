a12a6b8970a6e7869f013f5799d9bc9527e83e42
0xlny <lenny@419labs.io>
2023-07-07 17:25:26 +0400

    :art: Fix project card alignement

--- src/components/card/CardProject.tsx
+++ src/components/card/CardProject.tsx
@@ -70,6 +70,7 @@ function CardProject({ index, project }: CardProjectProps) {
   const indication = getIndicationText();
   return (
     <Flex
+      h="full"
       onClick={() => router.push({ pathname: `/projects/${project.id}` })}
       align="center"
       direction="column"
@@ -111,46 +112,62 @@ function CardProject({ index, project }: CardProjectProps) {
           </ChakraTag>
         )}
       </Box>
-      <Flex direction="column" align="center" mt="-50px">
-        <Avatar
-          size="xl"
-          name={name}
-          src={network.twitterImage || `/logos/${image}`}
-          border="2px solid white"
-          bg={getFallbackColor()}
-        />
-        <Text as="h6" fontSize="lg" fontWeight="bold" mt={4} textAlign="center">
-          {name}
-        </Text>
-        <HStack
-          spacing={0}
-          flexWrap="wrap"
-          justify="center"
-          shouldWrapChildren
-          mt={4}
-        >
-          {tags && tags.length > 0 ? (
-            tags.filter(pick(5)).map((tag) => {
-              // limit to 5 tags, as this looks nice and limits tags to 2 lines max
-              return (
-                <ChakraTag
-                  fontSize="xs"
-                  fontWeight="medium"
-                  borderRadius="xl"
-                  m={0.5}
-                  key={`project-${name}-tag-${tag.value}`}
-                >
-                  {t.tags[tag.value]}
-                </ChakraTag>
-              );
-            })
-          ) : (
-            <ChakraTag key={`project-${name}-tag-none`}>😕</ChakraTag>
-          )}
-        </HStack>
-        <Box my={4}>
-          <NetworkLogos network={network} />
-        </Box>
+      <Flex
+        flexGrow={1}
+        justify="space-between"
+        direction="column"
+        align="center"
+        mt="-50px"
+      >
+        <Flex direction="column" align="center">
+          <Avatar
+            size="xl"
+            name={name}
+            src={network.twitterImage || `/logos/${image}`}
+            border="2px solid white"
+            bg={getFallbackColor()}
+          />
+          <Text
+            as="h6"
+            fontSize="lg"
+            fontWeight="bold"
+            mt={4}
+            textAlign="center"
+          >
+            {name}
+          </Text>
+        </Flex>
+        <Flex flexGrow={1} direction="column" align="center" justify="space-between">
+          <HStack
+            spacing={0}
+            flexWrap="wrap"
+            justify="center"
+            shouldWrapChildren
+            mt={4}
+          >
+            {tags && tags.length > 0 ? (
+              tags.filter(pick(5)).map((tag) => {
+                // limit to 5 tags, as this looks nice and limits tags to 2 lines max
+                return (
+                  <ChakraTag
+                    fontSize="xs"
+                    fontWeight="medium"
+                    borderRadius="xl"
+                    m={0.5}
+                    key={`project-${name}-tag-${tag.value}`}
+                  >
+                    {t.tags[tag.value]}
+                  </ChakraTag>
+                );
+              })
+            ) : (
+              <ChakraTag key={`project-${name}-tag-none`}>😕</ChakraTag>
+            )}
+          </HStack>
+          <Box my={4}>
+            <NetworkLogos network={network} />
+          </Box>
+        </Flex>
       </Flex>
     </Flex>
   );
