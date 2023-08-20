2ac878bf3b3d44452516c9a893f974150164c585
Zypheraum <zypher@raum.network>
2023-08-20 22:33:54 +0000

    Updating RN Bridge Details

--- data/ecosystem.ts
+++ data/ecosystem.ts
@@ -2876,11 +2876,12 @@ export const allProjects: Array<Project> = [
     id: "101f06d5-595f-4254-ac9e-4bb6c7975b00",
     name: "RN Bridge",
     shortName: "RN Bridge",
-    description: "Seamless transfer of RN tokens from Polygon to StarkNet.",
+    description: 
+      "Raum Network's RN Bridge introduces an innovative solution for users seeking to seamlessly transfer their RN Tokens from the Polygon network to StarkNet. This bridge serves as a vital link, ensuring the smooth migration of tokens between these two distinct blockchain ecosystems. One of the standout features of the RN Bridge is its commitment to user convenience and inclusivity. While currently catering to RN Tokens between Polygon and StarkNet, the bridge is set to broaden its utility by incorporating major tokens such as ETH, USDC, USDT, DAI, and WBTC in forthcoming updates. This expansion promises to enhance the versatility and value of the bridge, catering to a wider range of users and use cases.",
     tags: ["all", "bridge", "defi"],
     image: "RN_Bridge.png",
     network: {
-      website: "https://raum.network",
+      website: "https://v1.bridge.raum.network/swap",
       github: "https://github.com/Raum-Network",
       twitter: "https://twitter.com/RaumNetwork",
       medium: "https://raumnetwork.medium.com",
